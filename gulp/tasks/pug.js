0be2e5cef796eddfb32172cd963e0051468d387c
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-08-12 01:31:06 +0300

    feat: add pugbem

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,6 +1,7 @@
 const htmlValidator = require('gulp-w3c-html-validator')
 const plumber = require('gulp-plumber')
 const pug = require('gulp-pug')
+const pugbem = require('gulp-pugbem')
 const argv = require('yargs').argv
 const gulpif = require('gulp-if')
 
@@ -8,7 +9,7 @@ module.exports = function pug2html() {
   return $.gulp
     .src('dev/pug/*.pug')
     .pipe(plumber())
-    .pipe(pug())
+    .pipe(pug({ plugins: [pugbem] }))
     .pipe(plumber.stop())
     .pipe(gulpif(argv.prod, htmlValidator()))
     .pipe($.gulp.dest('dist'))
